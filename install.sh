6ca8ba00241f564bf3494db3742da29fea3e3782
Matt McFarland <MattMcFarland@users.noreply.github.com>
2014-11-08 16:45:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,24 +10,27 @@ export PATH=$PATH:~/.local/lib/bin
 
 # Install Python 2.7.6
 cd $HOME/.src
-wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz && tar -xJvf *.xz && cd Py*
-./configure --prefix $HOME/.local 
-echo Installing Python 2.7.6 ... (Process may take 10-30 mins)
-make > $HOME/.src/log/Python-2.7.6.log && make install >> $HOME/.src/log/Python-2.7.6.log
+curl https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz > Python-2.7.6.tar.xz && tar -xJvf *.xz && cd Py*
+echo Configuring Python 2.7.6 ...
+./configure --prefix $HOME/.local > HOME/.src/log/Python-2.7.6.log
+echo Installing Python 2.7.6 ... Process may take 10-30 mins
+make >> $HOME/.src/log/Python-2.7.6.log && make install >> $HOME/.src/log/Python-2.7.6.log
 
 # Install NodeJS
 cd $HOME/.src
-wget http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz && tar -xzvf node*.gz && cd node*
-./configure --prefix $HOME/.local 
-echo Installing NodeJS 0.10.33 ... (Process may take 10-30 mins)
-make > $HOME/.src/log/node-v0.10.33.log && make install >> $HOME/.src/log/node-v0.10.33.log
+curl http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz > node-v0.10.33 && tar -xzvf node*.gz && cd node*
+echo Configuring NodeJS 0.10.33 ...
+./configure --prefix $HOME/.local > HOME/.src/log/node-v0.10.33.log
+echo Installing NodeJS 0.10.33 ... Process may take 10-30 mins
+make >> $HOME/.src/log/node-v0.10.33.log && make install >> $HOME/.src/log/node-v0.10.33.log
 
 # Install Ruby
 cd $HOME/.src
-wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz && tar -xzvf ruby-2.1.4.tar.gz && cd ruby-2.1.4
-./configure --prefix $HOME/.local 
-echo Installing Ruby 2.1.4 ... (Process may take 10-30 mins)
-make > $HOME/.src/log/ruby-2.1.4.log && make install >> $HOME/.src/log/ruby-2.1.4.log
+curl http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz > ruby-2.1.4 && tar -xzvf ruby-2.1.4.tar.gz && cd ruby-2.1.4
+echo Configuring Ruby 2.1.4 ...
+./configure --prefix $HOME/.local > $HOME/.src/log/ruby-2.1.4.log
+echo Installing Ruby 2.1.4 ... Process may take 10-30 mins
+make >> $HOME/.src/log/ruby-2.1.4.log && make install >> $HOME/.src/log/ruby-2.1.4.log
 
 # Setup PATH exports
 echo Configuring PATH variables
