24c97bb332ab46989b1baa113916d55bae50d263
Matt McFarland <MattMcFarland@users.noreply.github.com>
2014-11-08 16:05:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,43 @@
 #!bin/bash
 
-cd ~ && mkdir .src && mkdir .local && cd .src
+# Make basic scaffold
+cd $HOME
+mkdir .src && mkdir .local && mkdir .src/log && mkdir $HOME/.local/bin && mkdir $HOME/.local/lib/bin
 
-wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz  && tar xzvf *.xz && cd Py*
+# Set local bin right away
+export PATH=$PATH:~/.local/bin
+export PATH=$PATH:~/.local/lib/bin
 
-./configure --prefix ~/.local && make && make install
+# Install Python 2.7.6
+cd $HOME/.src
+wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz && tar xzvf *.xz && cd Py*
+./configure --prefix $HOME/.local 
+echo Installing Python 2.7.6 ... 
+make > $HOME/.src/log/Python-2.7.6.log && make install >> $HOME/.src/log/Python-2.7.6.log
 
-cd ~/.src
-
+# Install NodeJS
+cd $HOME/.src
 wget http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz && tar xzvf node*.gz && cd node*
+./configure --prefix $HOME/.local 
+echo Installing NodeJS 0.10.33 ...
+make > $HOME/.src/log/node-v0.10.33.log && make install >> $HOME/.src/log/node-v0.10.33.log
 
-./configure --prefix ~/.local && make && make install
+# Install Ruby
+cd $HOME/.src
+wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz && tar xzvf ruby-2.1.4.tar.gz && cd ruby-2.1.4
+./configure --prefix $HOME/.local 
+echo Installing Ruby 2.1.4 ...
+make > $HOME/.src/log/ruby-2.1.4.log && make install >> $HOME/.src/log/ruby-2.1.4.log
 
-cd ~/.src
-
-wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz && tar xzvf ruby*.gz && cd ruby*
-
-./configure --prefix ~/.local && make && make install && cd ~
-
-echo export PATH=$PATH:~/.local/bin >> ~/.ners_rc
+# Setup PATH exports
+echo Configuring PATH variables
+echo export PATH=$PATH:~/.local/bin > ~/.ners_rc
 echo export PATH=$PATH:~/.local/lib/bin >> ~/.ners_rc
 echo node --version && npm --version && ruby --version >> ~/.ners_rc
 echo source ~/.ners_rc >> ~/.bashrc
 source ~/.bashrc
 
 npm install express grunt bower -g
+chmod -R 700 .local
 
 echo Welcome to NERS
