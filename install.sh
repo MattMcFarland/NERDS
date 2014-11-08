1ef0670983cc99750d375a13663df9d0cc9e80f3
Matt McFarland <MattMcFarland@users.noreply.github.com>
2014-11-08 16:39:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,21 +12,21 @@ export PATH=$PATH:~/.local/lib/bin
 cd $HOME/.src
 wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz && tar -xJvf *.xz && cd Py*
 ./configure --prefix $HOME/.local 
-echo Installing Python 2.7.6 ... 
+echo Installing Python 2.7.6 ... (Process may take 10-30 mins)
 make > $HOME/.src/log/Python-2.7.6.log && make install >> $HOME/.src/log/Python-2.7.6.log
 
 # Install NodeJS
 cd $HOME/.src
 wget http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz && tar -xzvf node*.gz && cd node*
 ./configure --prefix $HOME/.local 
-echo Installing NodeJS 0.10.33 ...
+echo Installing NodeJS 0.10.33 ... (Process may take 10-30 mins)
 make > $HOME/.src/log/node-v0.10.33.log && make install >> $HOME/.src/log/node-v0.10.33.log
 
 # Install Ruby
 cd $HOME/.src
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.4.tar.gz && tar -xzvf ruby-2.1.4.tar.gz && cd ruby-2.1.4
 ./configure --prefix $HOME/.local 
-echo Installing Ruby 2.1.4 ...
+echo Installing Ruby 2.1.4 ... (Process may take 10-30 mins)
 make > $HOME/.src/log/ruby-2.1.4.log && make install >> $HOME/.src/log/ruby-2.1.4.log
 
 # Setup PATH exports
